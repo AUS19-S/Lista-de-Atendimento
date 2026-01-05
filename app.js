af7d85af8526c95180493d1ea95c1376ee84de09
Silanda <silanda@aluno.unilab.edu.br>
2026-01-05 16:29:05 -0300

    Fix equality check and update event listeners

--- app.js
+++ app.js
@@ -34,13 +34,13 @@ urgencia.addEventListener("click", () =>{
     let emAtendimento = document.getElementById("emAtendimento");
     let lista = document.getElementById("lista");
 
-    if(nome = "" || !isNaN(nome)){
+    if(nome == "" || !isNaN(nome)){
         alert("Por favor, precisamos do seu nome urgentemente.");
         return;
     }
     document.getElementById("nome").value = "";
 
-    variavel.unshift()
+    variavel.unshift(nome);
     let list =  "";
     for(x = 0; x < variavel.length; x++){
         list += (x + 1) + "." + variavel[x] + "\n";
@@ -48,4 +48,20 @@ urgencia.addEventListener("click", () =>{
     lista.textContent = list;
     emAtendimento.textContent = nome;
     emAtendimento.style.color = "red";
+})
+
+// Trabalhar com botão atender;
+let atender = document.getElementById("atender");
+atender.addEventListener("click", () =>{
+    let nome = document.getElementById("nome").value;
+    let lista = document.getElementById("lista");
+    let emAtendimento = document.getElementById("emAtendimento");
+    
+    variavel.shift(nome);
+    let list = "";
+    for(let x = 0; x < variavel.length; x++){
+        list += x + 1 + "." + variavel[x] + "\n";
+        }
+    lista.textContent = list;
+    emAtendimento.textContent = " ";
 })
